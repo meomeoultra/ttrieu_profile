2870c858814062fd0c299665eaa5b4d144cd9a26
meomeoultra <spkay703@gmail.com>
2025-09-04 10:34:56 +0700

    Update app.py, requirements.txt và Procfile

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -7,5 +8,7 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
 
