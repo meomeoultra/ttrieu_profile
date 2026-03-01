98aeeed51f1dad4a221640e1b1cddf42a3d3ac8a
meomeoultra <spkay703@gmail.com>
2026-03-01 14:51:26 +0700

    xoá html tet+ lan N

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+# Trang chủ → tự redirect về profile
 @app.route("/")
 def home():
-    return redirect("/tet")   # chỉ redirect 1 chiều
-
-@app.route("/tet")
-def tet():
-    return render_template("tet.html")
+    return redirect(url_for("profile"))
 
 @app.route("/profile")
 def profile():
@@ -17,5 +14,6 @@ def profile():
 @app.route("/hoa")
 def hoa():
     return render_template("hoa.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
