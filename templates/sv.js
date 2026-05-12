954efc3a107d2133e2ba7a7f78a54dc0c25e4de8
meomeoultra <spkay703@gmail.com>
2026-05-13 00:53:07 +0700

    add health route

---
+++ templates/sv.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.get("/health", (req, res) => {
+  res.send("OK");
+});
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "templates/index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
